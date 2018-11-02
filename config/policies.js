604b58e8897de5ff39148c3c17567f0bbaa96a1e
chellamit9x <chellamit9x@gmail.com>
2018-11-02 16:33:14 +0700

    a

---
+++ config/policies.js
@@ -0,0 +1,10 @@
+module.exports.policies = {
+  '*': 'login',
+  'Home/*': true,
+  'Client/*': true,
+  'Blogs/*': true,
+  'Communication/*': true,
+  'Restaurant/*': true,
+  'Tracking/*': true,
+  'Setting/*': true,
+};
